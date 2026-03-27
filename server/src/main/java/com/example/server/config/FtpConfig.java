4e85d897077128e944c2696ab5591aef101c853b
martindoychinov07 <martin.v.doychinov.2021@elsys-bg.org>
2026-03-27 19:12:42 +0200

    Ftp config

--- server/src/main/java/com/example/server/config/FtpConfig.java
+++ server/src/main/java/com/example/server/config/FtpConfig.java
@@ -1,4 +1,26 @@
 package com.example.server.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.ftp.session.DefaultFtpSessionFactory;
+
+@Configuration
 public class FtpConfig {
+
+    @Bean
+    public DefaultFtpSessionFactory ftpSessionFactory() {
+        DefaultFtpSessionFactory factory = new DefaultFtpSessionFactory();
+
+        factory.setHost("localhost");
+        factory.setPort(2121);
+
+        factory.setUsername("user");
+        factory.setPassword("12345");
+
+        factory.setClientMode(2);
+
+        factory.setConnectTimeout(10000);
+
+        return factory;
+    }
 }
